a529995b9fa0b4d57d224baca3a7dffa22f0b63f
israel tarazona <israeltarazona@gmail.com>
2019-02-22 10:12:36 -0500

    setting firebase and create service

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase'
+const conection = {
+  initialize: function () {
+    var config = {
+      apiKey: "AIzaSyAo226kJY6N6NyxvQpIH0WUtBD3UWkoaao",
+      authDomain: "arquitecturapragma.firebaseapp.com",
+      databaseURL: "https://arquitecturapragma.firebaseio.com",
+      projectId: "arquitecturapragma",
+      storageBucket: "",
+      messagingSenderId: "340869565697"
+    }
+    firebase.initializeApp(config)
+  }
+}
+
+export default conection
