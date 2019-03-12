429ea84fe5828469a3b8715af144dd30e12dd131
israel tarazona <israeltarazona@gmail.com>
2019-03-12 11:42:06 -0500

    modal

--- src/components/detailView/modal/index.js
+++ src/components/detailView/modal/index.js
@@ -9,46 +9,6 @@ import Modal from "react-native-modal";
 import Icon from 'react-native-vector-icons/FontAwesome'
 import styles from './style'
 
-renderButton = (text, props) => (
-  <TouchableOpacity onPress={() => props.isCloseModal()}>
-    <View style={styles.button}>
-      <Text>{text}</Text>
-    </View>
-  </TouchableOpacity>
-)
-
-renderModalContent = (props, torrents, title_long) => (
-  <View style={styles.modalContent}>
-    <View >
-      <Text style={styles.titleMovie}>{title_long}</Text>
-    </View>
-    {torrents.map((torrent, key) =>
-      <View key={key} style={styles.contanierOption}>
-        <Text style={styles.colorTitleOption}>Option {key}</Text>
-        <Text><Text style={styles.textBack}>Url:</Text> {torrent.url}</Text>
-        <View style={styles.option}>
-          <View>
-            <Text><Text style={styles.textBack}>Quality:</Text> {torrent.quality}</Text>
-            <Text><Text style={styles.textBack}>Type:</Text> {torrent.type}</Text>
-            <Text><Text style={styles.textBack}>Size:</Text> {torrent.size}</Text>
-          </View>
-          <View>
-            <TouchableNativeFeedback onPress={() => null}>
-              <View>
-                <Icon
-                  name="download"
-                  size={20}
-                  color="#00BCD4" />
-              </View>
-            </TouchableNativeFeedback>
-          </View>
-        </View>
-      </View>
-    )}
-     {this.renderButton('Close', props)}
-  </View>
-);
-
 function DetailBody(props) {
   const { torrents, title_long } = props.navigation.state.params
   return (
@@ -58,7 +18,39 @@ function DetailBody(props) {
         onBackdropPress={() => props.isCloseModal()}
         onSwipeComplete={() => props.isCloseModal()}
         swipeDirection="left">
-        {this.renderModalContent(props, torrents, title_long)}
+        <View style={styles.modalContent}>
+          <View >
+            <Text style={styles.titleMovie}>{title_long}</Text>
+          </View>
+          {torrents.map((torrent, key) =>
+            <View key={key} style={styles.contanierOption}>
+              <Text style={styles.colorTitleOption}>Option {key}</Text>
+              <Text><Text style={styles.textBack}>Url:</Text> {torrent.url}</Text>
+              <View style={styles.option}>
+                <View>
+                  <Text><Text style={styles.textBack}>Quality:</Text> {torrent.quality}</Text>
+                  <Text><Text style={styles.textBack}>Type:</Text> {torrent.type}</Text>
+                  <Text><Text style={styles.textBack}>Size:</Text> {torrent.size}</Text>
+                </View>
+                <View>
+                  <TouchableNativeFeedback onPress={() => null}>
+                    <View>
+                      <Icon
+                        name="download"
+                        size={20}
+                        color="#00BCD4" />
+                    </View>
+                  </TouchableNativeFeedback>
+                </View>
+              </View>
+            </View>
+          )}
+          <TouchableOpacity onPress={() => props.isCloseModal()}>
+            <View style={styles.button}>
+              <Text>close</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </Modal>
     </View>
   )
